74409afddd388fabbf348bc69d572c9ef8541171
Dmitry Nix <dmitry.nix@gmail.com>
2012-09-01 21:20:16 -0300

    Started with base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleCrud::Application.config.secret_token = 'dc22e1f16806409051b75d036204fae933dce2967ed5289bdc0b199483f7100dfe60c0065846352e739a44a95106a81398c959b21364d0f728fb97a44e45934e'
